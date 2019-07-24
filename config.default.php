f68702675de84e6eadb549ba70bd49ff0cfc87a8
King0625 <a7164265@gmail.com>
2019-07-24 11:49:38 +0800

    edit config.default.php

--- config.default.php
+++ config.default.php
@@ -7,3 +7,7 @@ define("DB_NAME", "{YOUR_DB_NAME}");
 // facebook graph-api 
 define("APP_ID", "{YOUR_APP_ID}");
 define("APP_SECRET", "{YOUR_APP_SECRET}");
+
+// Google reCaptcha v2
+define("SITE_KEY", "{YOUR_SITE_KEY}");
+define("SECRET_KEY", "{YOUR_SECRET_KEY}");
